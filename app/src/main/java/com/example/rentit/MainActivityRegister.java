5c37d2fc7755eb3aaca9b88df5660e737353d9f0
yoaveliashiv <yoavmshmuel2@gmail.com>
2020-12-07 14:14:51 +0200

    try phone

--- app/src/main/java/com/example/rentit/MainActivityRegister.java
+++ app/src/main/java/com/example/rentit/MainActivityRegister.java
@@ -3,6 +3,7 @@ package com.example.rentit;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,14 +15,22 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivityRegister extends AppCompatActivity {
     private FirebaseDatabase database;
@@ -31,11 +40,12 @@ public class MainActivityRegister extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private RegisterInformation registerInformation;
 
-    private Button registerButton;
+    private Button registerButton,buttonPhone,buttonSmsCode;
     private Button returnButton;
-    private EditText editTextEmail;
+    private EditText editTextEmail,editTextPhone,editTextSmsCode;
     private EditText editTextPass1;
     private EditText editTextPass2;
+    private  String mVerificationId;
 
 
     @Override
@@ -53,10 +63,47 @@ public class MainActivityRegister extends AppCompatActivity {
 
 
         registerButton = findViewById(R.id.register);
+        buttonSmsCode=findViewById(R.id.buttonCodeGo);
+        buttonPhone=findViewById(R.id.buttonRegisterPhone);
         returnButton = findViewById(R.id.mainPage);
+        editTextSmsCode=findViewById(R.id.editTextSmsCode);
+        editTextPhone=findViewById(R.id.editTextPhone);
         editTextEmail = findViewById(R.id.email);
         editTextPass1 = findViewById(R.id.editTextTextPassword);
         editTextPass2 = findViewById(R.id.password2);
+        buttonPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mobile = editTextPhone.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    editTextPhone.setError("Enter a valid mobile");
+                   editTextPhone.requestFocus();
+                   return;
+                }
+                else{
+                    mobile="+972"+mobile;
+                  sendVerificationCode(mobile);
+//                    editTextSmsCode.setVisibility(View.VISIBLE);
+//                    buttonSmsCode.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
+
+        buttonSmsCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = editTextSmsCode.getText().toString().trim();
+                if (code.isEmpty() || code.length() < 6) {
+                    editTextSmsCode.setError("Enter valid code");
+                    editTextSmsCode.requestFocus();
+                    return;
+                }
+                verifyVerificationCode(code);
+
+            }
+        });
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -144,6 +191,86 @@ public class MainActivityRegister extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    private void sendVerificationCode(String mobile) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + mobile,
+                60,
+                TimeUnit.SECONDS,
+                (Activity) TaskExecutors.MAIN_THREAD,
+                mCallbacks);
+    }
+
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new  PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            //Getting the code sent by SMS
+            String code = phoneAuthCredential.getSmsCode();
+
+            //sometime the code is not detected automatically
+            //in this case the code will be null
+            //so user has to manually enter the code
+            if (code != null) {
+                editTextSmsCode.setText(code);
+                //verifying the code
+                verifyVerificationCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(MainActivityRegister.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            mVerificationId = s;
+          //  mResendToken = forceResendingToken;
+        }
+    };
+    private void verifyVerificationCode(String code) {
+        //creating the credential
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+
+        //signing the user
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(MainActivityRegister.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //verification successful we will start the profile activity
+                            Intent intent = new Intent(MainActivityRegister.this, MainActivityPageUser.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);// TODO: all like this
+                            startActivity(intent);
+
+                        } else {
+
+                            //verification unsuccessful.. display an error message
+
+                            String message = "Somthing is wrong, we will fix it soon...";
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                message = "Invalid code entered...";
+                            }
+
+                            Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), message, Snackbar.LENGTH_LONG);
+                            snackbar.setAction("Dismiss", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+
+                                }
+                            });
+                            snackbar.show();
+                        }
+                    }
+                });
     }
 
 
